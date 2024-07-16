fe782fa379c785d8ccfffa16dd1b7f383de4af08
Alejandro <navarro.alejandro@outlook.es>
2024-07-16 00:03:24 -0600

    fix: modificando imagen responsiva

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -35,7 +35,7 @@ const Quiz = () => {
   } = useNextQuestion(isReset);
 
   return (
-    <section className="w-full min-h-screen overflow-hidden bg-gradient-to-tl from-bg-primary to-primary lg:bg-gradient-to-r lg:from-bg-primary lg:to-primary">
+    <section className="w-full h-screen bg-gradient-to-tl from-bg-primary to-primary md:overflow-hidden lg:bg-gradient-to-r lg:from-bg-primary lg:to-primary">
       <article className="flex flex-col justify-evenly items-center ">
         <div className="w-full h-28 flex flex-col justify-center text-center md:bg-primary/30 md:h-32 lg:w-4/5 lg:mb-5 lg:rounded-br-xl lg:rounded-bl-xl lg:shadow-xl lg:shadow-primary/60 xl:w-1/2">
           <h2 className="font-bold text-4xl mb-3 text-primary-text md:tracking-widest lg:text-5xl">
